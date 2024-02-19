4313ced5f0ce7253f64199eb105397db89622159
DoanKhiem <khiembi161219@gmail.com>
2024-02-19 14:18:58 +0700

    Save customer to database

--- resources/js/Pages/create.vue
+++ resources/js/Pages/create.vue
@@ -2,13 +2,16 @@
 import { useForm } from "@inertiajs/vue3";
 import {router} from "@inertiajs/vue3";
 
+defineProps({
+    errors: Object
+})
 const form = useForm({
     name: null,
     email: null,
     phone: null,
 })
 function submit () {
-    router.post('/customer', form)
+    router.post('/customers', form)
 }
 </script>
 
@@ -21,15 +24,18 @@ function submit () {
             <form @submit.prevent="submit">
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Name</label>
-                    <input type="email" id="name" v-model="form.name" class="form-control" aria-describedby="emailHelp">
+                    <input type="text" id="name" v-model="form.name" class="form-control">
+                    <div class="text-danger text-xs" v-if="errors.name">{{ errors.name }}</div>
                 </div>
                 <div class="mb-3">
                     <label for="exampleInputPassword1" class="form-label">Email</label>
                     <input type="email" v-model="form.email" class="form-control" id="email">
+                    <div class="text-danger text-xs" v-if="errors.email">{{ errors.email }}</div>
                 </div>
                 <div class="mb-3">
                     <label for="exampleInputPassword1" class="form-label">Phone</label>
                     <input type="number" v-model="form.phone" class="form-control" id="phone">
+                    <div class="text-danger text-xs" v-if="errors.phone">{{ errors.phone }}</div>
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
